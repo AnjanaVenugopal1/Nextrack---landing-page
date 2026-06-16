6b74cfcb87655831b8b43b369344e2e615ff69db
AnjanaVenugopal1 <157453579+AnjanaVenugopal1@users.noreply.github.com>
2026-06-16 17:18:24 +0530

    Remove Next.js build files and update gitignore

--- .next/dev/static/chunks/app_page_0zr2k9a.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/app_0dgjxpt._.js","static/chunks/node_modules_0b-x5do._.js"],
-    source: "dynamic"
-});
